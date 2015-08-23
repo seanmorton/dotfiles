0750694b26b7aaf08965d8d0b99d78897d44dfbb
Sean Morton <smorton517@gmail.com>
2015-08-23 00:41:42 -0500

    Add vim-hybrid to install script

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -5,12 +5,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
 git clone https://github.com/scrooloose/nerdtree.git
-
-git clone https://github.com/gcmt/taboo.vim.git
-
-git clone https://github.com/xolox/vim-session.git
+git clone https://github.com/w0ng/vim-hybrid.git
 git clone https://github.com/xolox/vim-misc.git
-
 git clone https://github.com/wesQ3/vim-windowswap.git
-
+git clone https://github.com/gcmt/taboo.vim.git
+git clone https://github.com/xolox/vim-session.git
 git clone https://github.com/terryma/vim-smooth-scroll.git
