3f01995c24a23c93218db5bb6b43e7b7927b9cba
Sean Morton <sean@mercury.com>
2021-08-25 12:03:10 -0700

    Update vim plugins

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -3,11 +3,8 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
-git clone https://github.com/5outh/yesod-routes.vim.git
-git clone https://github.com/LnL7/vim-nix.git
 git clone https://github.com/junegunn/fzf.vim.git
 git clone https://github.com/ledger/vim-ledger.git
-git clone https://github.com/neovimhaskell/haskell-vim.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/seanmorton/vim-tomorrow-night-eighties.git
 git clone https://github.com/tpope/vim-dispatch.git
@@ -20,3 +17,4 @@ git clone https://github.com/yuttie/comfortable-motion.vim.git
 # git clone https://github.com/craigemery/vim-autotag.git
 # git clone https://github.com/thoughtbot/vim-rspec.git
 # git clone https://github.com/othree/yajs.vim.git
+# git clone https://github.com/neovimhaskell/haskell-vim.git
