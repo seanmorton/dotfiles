cc059b1569209d82d31a70886de5dc5b281ce9cc
Sean Morton <seanmorton@hey.com>
2022-10-13 12:02:52 -0700

    Add vim-terraform

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -4,6 +4,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
 git clone https://github.com/fatih/vim-go.git
+git clone https://github.com/hashivim/vim-terraform.git
 git clone https://github.com/junegunn/fzf.vim.git
 git clone https://github.com/ledger/vim-ledger.git
 git clone https://github.com/scrooloose/nerdtree.git
@@ -12,7 +13,6 @@ git clone https://github.com/tpope/vim-dispatch.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/w0rp/ale.git
 git clone https://github.com/yuttie/comfortable-motion.vim.git
-
 
 # Previously used and worth keeping for reference
 # git clone https://github.com/mileszs/ack.vim.git
