5b98801599c7ae5bb1294cb11e9a42c29febd08e
Sean Morton <sean@pelo.tech>
2024-10-24 23:18:21 -0500

    tmux popup: session per window

--- scripts/tmux_popup.sh
+++ scripts/tmux_popup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 width=${2:-75%}
 height=${2:-75%}
-tmux popup -d '#{pane_current_path}' -b padded -w $width -h $height -E "$SHELL"
+if [[ "$(tmux display-message -p "#{session_name}")" == "float"* ]]; then
+    tmux detach-client
+else
+    WINDOW_IDX=$(tmux display -p '#{active_window_index}')
+    tmux popup -d '#{pane_current_path}' -b padded -w $width -h $height -E "tmux attach -t float-$WINDOW_IDX || tmux new -s float-$WINDOW_IDX"
+fi
