61440d1e3066d1efc21bdfc43bf4186bd44cf4fa
Sean Morton <sean@pelo.tech>
2024-10-24 22:29:26 -0500

    borderless terminals

--- scripts/tmux_popup.sh
+++ scripts/tmux_popup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 width=${2:-75%}
 height=${2:-75%}
-tmux popup -d '#{pane_current_path}' -w $width -h $height -E "$SHELL"
+tmux popup -d '#{pane_current_path}' -b padded -w $width -h $height -E "$SHELL"
