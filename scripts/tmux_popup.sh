16191f7438f6d19287d10a42e0f44d2f78d20e0d
Sean Morton <sean@pelo.tech>
2024-10-25 14:29:04 -0500

    tmux: popup quake style

--- scripts/tmux_popup.sh
+++ scripts/tmux_popup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-width=${2:-75%}
-height=${2:-75%}
+# Thanks to https://blog.meain.io/2020/tmux-flating-scratch-terminal/
+width=${2:-100%}
+height=${2:-65%}
 if [[ "$(tmux display-message -p "#{session_name}")" == "float"* ]]; then
     tmux detach-client
 else
     WINDOW_IDX=$(tmux display -p '#{active_window_index}')
-    tmux popup -d '#{pane_current_path}' -b padded -w $width -h $height -E "tmux attach -t float-$WINDOW_IDX || tmux new -s float-$WINDOW_IDX"
+    tmux popup -d '#{pane_current_path}' -b simple -y 0 -w $width -h $height -E "tmux attach -t float-$WINDOW_IDX || tmux new -s float-$WINDOW_IDX"
 fi
