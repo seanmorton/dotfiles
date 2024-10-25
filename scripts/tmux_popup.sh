d25c2b81c125819dd90814b72ce11c39103a28c3
Sean Morton <sean@pelo.tech>
2024-10-24 22:14:31 -0500

    tmux: simplify popup and clipboard

--- scripts/tmux_popup.sh
+++ scripts/tmux_popup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Thanks to https://blog.meain.io/2020/tmux-flating-scratch-terminal/
 width=${2:-75%}
 height=${2:-75%}
-if [ "$(tmux display-message -p -F "#{session_name}")" = "float" ];then
-    tmux detach-client
-else
-    tmux popup -d '#{pane_current_path}' -w$width -h$height -E "tmux attach -t float || tmux new -s float"
-fi
+tmux popup -d '#{pane_current_path}' -w $width -h $height -E "$SHELL"
