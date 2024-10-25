2b7f8172c635f9f8fdc2cdf4e90540fa1f709f9b
Sean Morton <sean@pelo.tech>
2024-10-25 16:20:37 -0500

    tmux: popup per session->window

--- scripts/tmux_popup.sh
+++ scripts/tmux_popup.sh
@@ -2,9 +2,10 @@
 # Thanks to https://blog.meain.io/2020/tmux-flating-scratch-terminal/
 width=${2:-100%}
 height=${2:-65%}
-if [[ "$(tmux display-message -p "#{session_name}")" == "float"* ]]; then
+if [[ "$(tmux display -p "#{session_name}")" == "float"* ]]; then
     tmux detach-client
 else
+    SESSION_NAME=$(tmux display -p '#{session_name}')
     WINDOW_IDX=$(tmux display -p '#{active_window_index}')
-    tmux popup -d '#{pane_current_path}' -b simple -y 0 -w $width -h $height -E "tmux attach -t float-$WINDOW_IDX || tmux new -s float-$WINDOW_IDX"
+    tmux popup -d '#{pane_current_path}' -b simple -y 0 -w $width -h $height -E "tmux attach -t float-$SESSION_NAME-$WINDOW_IDX || tmux new -s float-$SESSION_NAME-$WINDOW_IDX"
 fi
